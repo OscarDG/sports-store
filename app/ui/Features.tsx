8fe92ce38f07c6061a5b7a1dbcc5ae07be6095f3
Oscar Guerrero <oscard.guerrerom@outlook.com>
2025-03-15 10:41:29 -0500

    style(features): included a banner in the features with its styles

--- app/ui/Features.tsx
+++ app/ui/Features.tsx
@@ -1,10 +1,29 @@
 import FeaturesCart from "./FeaturesCart";
-
+import FabricIcon from "@/public/fabric";
+import RecycleIcon from "@/public/recycling";
+import TechIcon from "@/public/technology";
 
 export default function Features(){
     return (
-        <section className="relative flex flex-col md:flex-row md:justify-evenly justify-center items-center w-[100%] h-auto">
-            <FeaturesCart />
+        <section className="relative flex flex-col justify-evenly items-center w-[100%] h-[230vh] md:h-[150vh]">
+            <div className="relative flex flex-col md:flex-row justify-center md:justify-evenly items-center w-[95%] h-auto">
+                <FeaturesCart />
+            </div>
+            <div className="relative flex flex-row flex-wrap justify-evenly items-center w-[100%] h-[40%] bg-[url(@/public/sunset.webp)] bg-center bg-no-repeat bg-cover">
+                <span className="flex flex-col justify-center items-center w-[20%] md:w-[10%] h-[35%] text-mainwhite md:text-[1.5rem] text-[1rem] gap-2 z-10">
+                    <FabricIcon width={'100%'} height={'100%'}/>
+                    Design
+                </span>
+                <span className="flex flex-col justify-center items-center w-[20%] md:w-[10%] h-[35%] text-mainwhite md:text-[1.5rem] text-[1rem] gap-2 z-10">
+                    <RecycleIcon width={'100%'} height={'100%'} />
+                    Technology
+                </span>
+                <span className="flex flex-col justify-center items-center w-[20%] md:w-[10%] h-[35%] text-mainwhite md:text-[1.5rem] text-[1rem] gap-2 z-10">
+                    <TechIcon width={'100%'} height={'100%'}/>
+                    Technology
+                </span>
+            <div className="absolute inset-0 bg-black/30"></div>
+            </div>
         </section>
     )
 }
