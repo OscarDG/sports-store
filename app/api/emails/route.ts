cf0eb5faaee950c9e49a54e3752bc5d8fec4f511
Oscar Guerrero <oscard.guerrerom@outlook.com>
2025-03-17 18:22:07 -0500

    Fix(newsletter): Fix typescript error

--- app/api/emails/route.ts
+++ app/api/emails/route.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/extension';
 
 const prisma = new PrismaClient();
 
@@ -25,7 +25,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             });
 
             return res.status(200).json({ message: "Subscription successful!" });
-        }catch(error) {
+        }catch(_error) {
             return res.status(500).json({ error: "Internal Server Error" });
         }
     }
