eca354a5ca956ee404307a38bf5e8f4f6eca9f9d
Oscar Guerrero <oscard.guerrerom@outlook.com>
2025-03-17 20:07:13 -0500

    style(newsletter): add styles to validation messages

--- app/ui/Newsletter.tsx
+++ app/ui/Newsletter.tsx
@@ -1,4 +1,5 @@
 'use client';
+import clsx from "clsx";
 import { monaSans } from "../fonts"
 import { useState } from 'react'
 
@@ -42,7 +43,7 @@ export default function Newsletter(){
                 <h3 className={`${monaSans.className} w-[60%] h-auto text-[1.3rem] md:text-[1.5rem] font-medium`}>Subscribe to our newsletter</h3>
                 <input className='w-[90%] h-[18%] pl-[5%] border-1 border-black rounded-sm focus:border-mainred focus:outline-none' type="email" name='email' autoComplete="on" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
                 <button name='newsletter' type='submit' className='w-[40%] h-[20%] md:h-[15%] text-mainwhite bg-mainred rounded-sm cursor-pointer'>Subscribe</button>
-                {message && <p className="text-sm mt-2">{message}</p>}
+                {message && <p className={clsx(`flex flex-col justify-center w-[50%] h-[10%] text-sm mt-2 text-mainwhite text-center`, message.includes('successful!')? 'bg-maingreen': 'bg-mainred', `rounded-3xl`)}>{message}</p>}
             </form>
     )
 }
