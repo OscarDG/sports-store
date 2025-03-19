4977cdbecdd73a3e08cc5e54ae7fbe43ca46d8a7
Oscar Guerrero <oscard.guerrerom@outlook.com>
2025-03-19 13:57:12 -0500

    Styles(StoreHeader): Update store header styles

--- app/ui/SearchBar.tsx
+++ app/ui/SearchBar.tsx
@@ -2,9 +2,9 @@ import Lookicon from "@/public/look"
 
 export default function SearchBar(){
     return (
-        <div className='search_bar absolute top-30 md:top-8 flex flex-row w-[80%] md:w-[50%] h-[20%] md:h-[25%]'>
-            <span className="flex justify-center items-center w-[30px] h-[30px] mr-3"><Lookicon width={20} height={20} fill="#F45B69"/></span>
-            <input className='w-[100%] h-[100%] pl-4 focus:bg-mainred/30 rounded-2xl border-1 border-mainred focus:border-mainred  focus:border-1 outline-none' type="text" placeholder="Look your products"></input>
+        <div className='search_bar absolute top-28 md:top-8 flex flex-row justify-center items-center w-[80%] md:w-[50%] h-[20%] md:h-[25%] rounded-[20px] border-1 border-mainred'>
+            <span className="flex absolute left-3 justify-center items-center w-[30px] h-[30px] mr-3"><Lookicon width={20} height={20} fill="#F45B69"/></span>
+            <input className='w-[100%] h-[100%] pl-15 focus:bg-mainred/30 rounded-2xl border-0 outline-none' type="text" placeholder="Look your products"></input>
         </div>
     )
 }
--- app/ui/StoreHeader.tsx
+++ app/ui/StoreHeader.tsx
@@ -14,13 +14,13 @@ type headerProducts = {
 export default function StoreHeader({products}:headerProducts){
 
     return (
-        <header className={clsx(`${monaSans.className} relative flex flex-col items-center w-[100%] h-[42vh] md:h-[25vh] pt-5 shadow-sm`)}>
+        <header className={clsx(`${monaSans.className} relative flex flex-col items-center w-[100%] h-[40vh] md:h-[25vh] pt-5 shadow-sm`)}>
             <nav className="header_navbar flex flex-col md:flex-row md:justify-between items-center w-[95%] h-[35%] md:h-[50%] p-2">
-                <div className="header_buttons absolute top-7 md:bottom-2 md:right-15 flex flex-row items-center w-[80%] md:w-[20%] h-[25%] md:h-[30%]">
-                    <div className="absolute w-[40%] h-[80%]">
+                <div className="header_buttons absolute top-2 md:top-7 md:right-15 flex flex-row items-center w-[80%] md:w-[20%] h-[25%] md:h-[32%]">
+                    <div className="absolute w-[40%] h-[60%]">
                         <BwButton width={100} height={100} href="/" target="_self" text="Go back"/>
                     </div>
-                    <div className="absolute right-0 flex flex-col justify-center items-center w-[20%] md:w-[18%] max-w-[75px] md:max-w-[70px] min-w-[40px] h-[90%] md:h-[90%] rounded-[100%] bg-mainred transition duration-200 ease-in hover:scale-105">
+                    <div className="absolute right-0 flex flex-col justify-center items-center w-[17%] md:w-[18%] max-w-[65px] md:max-w-[70px] min-w-[40px] h-[70%] md:h-[80%] rounded-[100%] bg-mainred transition duration-200 ease-in hover:scale-105">
                         <span className={clsx(products.length <= 0? `hidden`:`absolute flex justify-center items-center text-[0.6rem] text-mainwhite font-medium top-[-5px] right-[0px] w-[15px] h-[15px] rounded-[100%] bg-maingreen`)}>{products.length}</span>
                         <button className='flex justify-center items-center w-[60px] md:w-[40px] h-[60px] md:h-[40px] cursor-pointer'><CartIcon width={'60%'} height={'60%'} stroke="#ffffff"/></button>
                     </div>
