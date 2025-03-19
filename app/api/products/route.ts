7871c514685fccee2efee67f45a3375985c276d9
Oscar Guerrero <oscard.guerrerom@outlook.com>
2025-03-18 23:26:37 -0500

    Fix(products Route): Update the check function for checking available products

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -10,7 +10,7 @@ export async function GET() {
     }));
 
     return new Response(
-        JSON.stringify(products),
+        JSON.stringify(formattedProducts),
         {status: 200, headers: { "Content-Type": "application/json" }}
     );
    }catch(error){
