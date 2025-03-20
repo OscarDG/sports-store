e618294acfbc84c244130c620c5c2c9debd9e17f
Oscar Guerrero <oscard.guerrerom@outlook.com>
2025-03-19 22:48:57 -0500

    Fix Type error in useFillter

--- app/hooks/useFilter.tsx
+++ app/hooks/useFilter.tsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
 import { FiltersContext } from '@/app/context/filterContext'
+import {Products} from '@/app/types'
 
 
 export function useFilter(){
 
     const {filters, setFilters} = useContext(FiltersContext)//trae los filters del contexto - consume el contexto
 
-    const filterProducts = (initialProducts) => {
+    const filterProducts = (initialProducts: Products[]) => {
         return initialProducts.filter(product => {
 
             const categoryMatch = filters.category === 'All' || product.category === filters.category
