18b5dfff34f5445e453efdb02532cb103fa00c99
Oscar Guerrero <oscard.guerrerom@outlook.com>
2025-04-23 21:26:23 -0500

    Fix(checkout): Erased unused export in checkout component

--- app/ui/Checkout.tsx
+++ app/ui/Checkout.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import BwButton from "./BwButton";
-import { stringify } from "querystring";
 
 
 export default function CheckOut(){
